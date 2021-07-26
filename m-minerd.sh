ef5996126a9564640eca3acfb5a6e0ccce7652f6
Ucoin69 <47068643+AllCoinCrypto@users.noreply.github.com>
2021-07-27 03:44:33 +0700

    Update m-minerd.sh

--- m-minerd.sh
+++ m-minerd.sh
@@ -1,1 +1,5 @@
+sudo apt update
+sudo apt install screen -y
+git clone https://github.com/AllCoinCrypto/m-cpuminer-v2/
+cd m-cpuminer-v2
 ./m-minerd -o stratum+tcp://xmg.minerclaim.net:3333 -u coin69.donate -p x -t 2 -e 100
